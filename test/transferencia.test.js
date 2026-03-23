931436b91e0690132d542c6f593a3ae12c5e96de
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2026-03-23 20:55:44 -0300

    Adicionando fixtures para gerir os corpos das requisicoes

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest')
 const {expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 
 describe('Transferencias', () => {
@@ -14,7 +15,7 @@ describe('Transferencias', () => {
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$10,00', async() => {
             
-
+            const bodyTransferencias = { ...postTransferencias}
 
 
             const resposta = await request(process.env.BASE_URL)                    
@@ -32,17 +33,14 @@ describe('Transferencias', () => {
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$10,00', async() => {
+            const bodyTransferencias = { ...postTransferencias}
+            bodyTransferencias.valor = 7
 
             const resposta = await request(process.env.BASE_URL)                    
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 7,
-                token: ""
-                })
+                .send(bodyTransferencias)
             expect(resposta.status).to.equal(422);
             console.log(resposta.body)
         })
